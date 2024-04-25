fbc5714c15fa5f51c6674c68f7a21728a77b9089
marik <letschuka@gmail.com>
2024-04-25 13:30:57 +0300

    resolve bugs with statis files

--- blast_courses/settings.py
+++ blast_courses/settings.py
@@ -101,10 +101,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 ]
 
 # Default primary key field type
