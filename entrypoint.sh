80bc5bffbc7d253f2f9dd4a77a02c45ccac19f31
Yura0Bodnar <yuriy575721@gmail.com>
2024-04-25 23:36:51 +0300

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 # Зупинити виконання скрипту при помилці
 set -e
 
-# Запуск сервера Django за допомогою gunicorn, використовуючи змінні середовища для конфігурації
-exec gunicorn blast_courses.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers ${GUNICORN_WORKERS:-3}
+# Запуск сервера Django без використання Gunicorn
+exec python manage.py runserver 0.0.0.0:${PORT:-8000}
