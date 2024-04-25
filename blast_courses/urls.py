1b8a16e885596a770e1b9212f24ddcd04159602a
Yura0Bodnar <yuriy575721@gmail.com>
2024-04-25 23:12:33 +0300

    added media_urls in url.py

--- blast_courses/urls.py
+++ blast_courses/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path("auth/", include("djoser.urls.jwt")),
 
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.STATIC_URL)
+] + static(settings.STATIC_URL) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
