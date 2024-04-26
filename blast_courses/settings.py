b8c1cf3084af4d02e49e4a5571b8d39d0fe8d26b
marik <letschuka@gmail.com>
2024-04-26 15:46:18 +0300

    add lib django-cors-headers

--- blast_courses/settings.py
+++ blast_courses/settings.py
@@ -20,10 +20,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'https://blast-courses-cfqbzl23vq-lm.a.run.app',
-)
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
