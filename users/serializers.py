5d63ef612df158fda7af8b3bf8295d9b3df72804
marik <letschuka@gmail.com>
2024-04-27 13:36:18 +0300

    add custom login auth

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
@@ -20,11 +20,39 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         return make_password(value)
 
     def create(self, validated_data):
-        user_type = validated_data.pop('user_type')  # Видаляємо user_type з validated_data
+        user_type = validated_data.pop('user_type')
         user = User.objects.create_user(**validated_data)
-        user.user_type = user_type  # Присвоюємо значення user_type
+        user.user_type = user_type
         user.save()
         return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get("email", None)
+        password = data.get("password", None)
+
+        if email is None:
+            raise serializers.ValidationError("An email address is required to log in.")
+
+        if password is None:
+            raise serializers.ValidationError("A password is required to log in.")
+
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                "A user with this email and password was not found."
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError("This user has been deactivated.")
+
+        return {"email": user.email, "username": user.username, "token": user.token}
 
 
 class UserSerializer(serializers.ModelSerializer):
