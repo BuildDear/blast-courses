35ffa6149a5e2ad41b415023537fac30219dbe8c
marik <letschuka@gmail.com>
2024-04-28 14:35:17 +0300

    update corsheaders rules

--- blast_courses/settings.py
+++ blast_courses/settings.py
@@ -19,6 +19,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1",
     "http://localhost:3000",
+    "http://blast-courses-cfqbzl23vq-lm.a.run.app",
 ]
 
 INSTALLED_APPS = [
